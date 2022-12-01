d38292b919669ed67a7a194ddc67a65079511905
yuvaraj <62199501+punagalayuvaraju@users.noreply.github.com>
2022-12-01 15:57:38 +0530

    grid options

--- src/app/table-list/table-list.component.ts
+++ src/app/table-list/table-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GridOptions } from 'ag-grid-community';
 @Component({
   selector: 'app-table-list',
   templateUrl: './table-list.component.html',
@@ -8,11 +9,22 @@ export class TableListComponent implements OnInit {
 
   constructor() { }
 
-  columnDefs = [{ field: "make" }, { field: "model" }, { field: "price" }];
+  gridOptions: GridOptions = {
+    defaultColDef: {
+      sortable: true
+    }
+  }
+
+  columnDefs = [{ field: "make" }, { field: "model" }, { field: "price" },
+  { field: "make" }, { field: "model" }, { field: "price" }, { field: "price" }];
 
   rowData = [
     { make: "Toyota", model: "Celica", price: 35000 },
     { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxter", price: 72000 },
+    { make: "Toyota", model: "Celica", price: 35000 },
+    { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxter", price: 72000 },
     { make: "Porsche", model: "Boxter", price: 72000 }
   ];
 
